79a5972629135aad4dc7fa1bf851b43009c2c829
Amine AL KADERI <amindeed@users.noreply.github.com>
2018-02-11 11:00:13 +0000

    Updated plugin info and name
    - New name: Admin NoReCAPTCHA
    - New URL: https://github.com/amindeed/YOURLS-Admin-NoReCAPTCHA
    - Replaced all "adminnocaptcha" occurrences with "adminnorecaptcha"

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: Admin NoCaptcha
-Plugin URI: https://github.com/amindeed/admin-nocaptcha
+Plugin Name: Admin NoReCAPTCHA
+Plugin URI: https://github.com/amindeed/YOURLS-Admin-NoReCAPTCHA
 Description: Enables Google's No CAPTCHA reCAPTCHA on Admin login screen. A port to Google ReCAPTCHA v2.0 of @armujahid's "Admin reCaptcha" plugin.
 Version: 1.0
 Author: Amindeed
@@ -10,10 +10,10 @@ Author URI: https://github.com/amindeed
 
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-yourls_add_action( 'pre_login_username_password', 'adminnocaptcha_validatereCaptcha' );
+yourls_add_action( 'pre_login_username_password', 'adminnorecaptcha_validatereCaptcha' );
 
 // Validates reCaptcha
-function adminnocaptcha_validatereCaptcha()
+function adminnorecaptcha_validatereCaptcha()
 {
 	include('captcha.php'); 
 	if ($resp != null && $resp->isSuccess()) 
@@ -31,57 +31,57 @@ function adminnocaptcha_validatereCaptcha()
 }
 
 // Register plugin on admin page
-yourls_add_action( 'plugins_loaded', 'adminnocaptcha_init' );
-function adminnocaptcha_init() {
-    yourls_register_plugin_page( 'adminnocaptcha', 'Admin NoCaptcha Settings', 'adminnocaptcha_config_page' );
+yourls_add_action( 'plugins_loaded', 'adminnorecaptcha_init' );
+function adminnorecaptcha_init() {
+    yourls_register_plugin_page( 'adminnorecaptcha', 'Admin NoCaptcha Settings', 'adminnorecaptcha_config_page' );
 }
 
 // The function that will draw the config page
-function adminnocaptcha_config_page() {
-    	 if( isset( $_POST['adminnocaptcha_public_key'] ) ) {
-	        yourls_verify_nonce( 'adminnocaptcha_nonce' );
-	        adminnocaptcha_save_admin();
+function adminnorecaptcha_config_page() {
+    	 if( isset( $_POST['adminnorecaptcha_public_key'] ) ) {
+	        yourls_verify_nonce( 'adminnorecaptcha_nonce' );
+	        adminnorecaptcha_save_admin();
 	    }
     
-    $nonce = yourls_create_nonce( 'adminnocaptcha_nonce' );
-    $pubkey = yourls_get_option( 'adminnocaptcha_pub_key', "" );
-    $privkey = yourls_get_option( 'adminnocaptcha_priv_key', "" );
+    $nonce = yourls_create_nonce( 'adminnorecaptcha_nonce' );
+    $pubkey = yourls_get_option( 'adminnorecaptcha_pub_key', "" );
+    $privkey = yourls_get_option( 'adminnorecaptcha_priv_key', "" );
     echo '<h2>Admin NoCaptcha plugin settings</h2>';
     echo '<form method="post">';
     echo '<input type="hidden" name="nonce" value="' . $nonce . '" />';
-    echo '<p><label for="adminnocaptcha_public_key">reCaptcha site key: </label>';
-    echo '<input type="text" id="adminnocaptcha_public_key" name="adminnocaptcha_public_key" value="' . $pubkey . '"></p>';  
-    echo '<p><label for="adminnocaptcha_private_key">reCaptcha secret key: </label>';
-    echo '<input type="text" id="adminnocaptcha_private_key" name="adminnocaptcha_private_key" value="' . $privkey . '"></p>';
+    echo '<p><label for="adminnorecaptcha_public_key">reCaptcha site key: </label>';
+    echo '<input type="text" id="adminnorecaptcha_public_key" name="adminnorecaptcha_public_key" value="' . $pubkey . '"></p>';  
+    echo '<p><label for="adminnorecaptcha_private_key">reCaptcha secret key: </label>';
+    echo '<input type="text" id="adminnorecaptcha_private_key" name="adminnorecaptcha_private_key" value="' . $privkey . '"></p>';
     echo '<input type="submit" value="Save"/>';
     echo '</form>';
 
 }
 
 // Save reCaptcha keys in database 
-function adminnocaptcha_save_admin()
+function adminnorecaptcha_save_admin()
 {
-	$pubkey = $_POST['adminnocaptcha_public_key'];
-	$privkey = $_POST['adminnocaptcha_private_key'];
-	if ( yourls_get_option( 'adminnocaptcha_pub_key' ) !== false ) {
-        yourls_update_option( 'adminnocaptcha_pub_key', $pubkey );
+	$pubkey = $_POST['adminnorecaptcha_public_key'];
+	$privkey = $_POST['adminnorecaptcha_private_key'];
+	if ( yourls_get_option( 'adminnorecaptcha_pub_key' ) !== false ) {
+        yourls_update_option( 'adminnorecaptcha_pub_key', $pubkey );
     } 
 	else {
-        yourls_add_option( 'adminnocaptcha_pub_key', $pubkey );
+        yourls_add_option( 'adminnorecaptcha_pub_key', $pubkey );
     }
-	if ( yourls_get_option( 'adminnocaptcha_priv_key' ) !== false ) {
-        yourls_update_option( 'adminnocaptcha_priv_key', $privkey );
+	if ( yourls_get_option( 'adminnorecaptcha_priv_key' ) !== false ) {
+        yourls_update_option( 'adminnorecaptcha_priv_key', $privkey );
     } 
 	else {
-        yourls_add_option( 'adminnocaptcha_priv_key', $privkey );
+        yourls_add_option( 'adminnorecaptcha_priv_key', $privkey );
     }
     echo "Saved";
 }
 
 // Add the JavaScript for reCaptcha widget
-yourls_add_action( 'html_head', 'adminnocaptcha_addjs' );
-function adminnocaptcha_addjs() {
-	$siteKey = yourls_get_option( 'adminnocaptcha_pub_key' );
+yourls_add_action( 'html_head', 'adminnorecaptcha_addjs' );
+function adminnorecaptcha_addjs() {
+	$siteKey = yourls_get_option( 'adminnorecaptcha_pub_key' );
 	?>
 	<script type="text/javascript">
 	//JQuery function to add div for reCaptcha widget and load js only on login screen
